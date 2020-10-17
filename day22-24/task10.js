69f2cff9ab1b46c9a5c1bce20618afb86f1cfefa
ache123 <631346398@qq.com>
2020-10-17 19:39:09 +0800

    Refactor task10

--- day22-24/task10.js
+++ day22-24/task10.js
@@ -34,10 +34,43 @@ var menuArr = [
     [8, "Area2-2-1", 6],
 ];
 
-var menuObject = new Object();
-var menuObject = {};
-for (var i = 0; i < menuArr.length; i++) {
-    var tmp = {};
-    tmp.name = menuArr[i][1];
-    
+
+//以下逻辑参考他人代码
+
+
+//妙在用了数组转存，以及引用类型
+
+function toObject(arr) {
+    var menuObject = {};
+    var storeArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        //既然是一个对象，首先要把每组数据转存下来
+        var temp = {
+            name: arr[i][1]
+            //subMenu: {}
+        }
+        //然后把他放入一个数组中
+        storeArr.push(temp);
+        //最后再考虑把它放在哪个对象里面
+        if (arr[i][2] == -1) {
+            menuObject['"' +arr[i][0] +'"'] = temp;
+        } else {
+            var father = arr[i][2];
+            storeArr[father - 1]["subMenu"] = {};
+            storeArr[father - 1]["subMenu"]['"'+ arr[i][0] +'"'] = temp;
+
+            /*
+                如果这里不分两步创建而是直接
+                storeArr[father - 1]["subMenu"]['"'+ arr[i][0] +'"'] = temp;
+                会导致最后一个属性无法创建
+                使用中括号语法创建属性，最多创建一个,因为前面那个就是undefined
+                除非在对象初始化的时候写subMenu: {}
+            */
+        }
+    }
+
+    return menuObject;
 }
+
+toObject(menuArr);
+
